4782a23360659f04f860902cdb3f21e9b86fa410
moop250 <hugo.libine@proton.me>
2025-06-10 18:33:40 +0200

    what was i thinking?

--- inc/core.hpp
+++ inc/core.hpp
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 19:32:40 by hlibine           #+#    #+#             */
-/*   Updated: 2025/06/04 19:51:14 by hlibine          ###   ########.fr       */
+/*   Updated: 2025/06/10 18:30:27 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ class Core {
 
 	private:
 		int		configFd;
-		void	parseCore(int fd);
-		std::map<std::string, t_coreSettings>	&serverConfigs;
+		std::map<std::string, t_coreSettings>	parseCore(char *file);
+		std::map<std::string, t_coreSettings>	serverConfigs;
 
 	public:
 		Core(int fd);
--- srcs/parsing/parser.cpp
+++ srcs/parsing/parser.cpp
@@ -6,7 +6,24 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 19:30:52 by hlibine           #+#    #+#             */
-/*   Updated: 2025/06/04 19:30:56 by hlibine          ###   ########.fr       */
+/*   Updated: 2025/06/10 18:33:23 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../inc/core.hpp"
+#include <fstream>
+#include <stdexcept>
+
+std::map<std::string, t_coreSettings> Core::parseCore(char *file) {
+
+	std::ifstream f(file);
+	if (!f.is_open())
+		throw std::runtime_error("Could not open config file");
+
+	std::map<std::string, t_coreSettings>	coreSett;
+
+
+	f.close();
+
+	return coreSett;
+}
