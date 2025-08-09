bacdfb2177d68fd7d849ce7b4a2962c4eae6d6a0
Noopy <seitealexander@gmail.com>
2025-08-09 10:02:02 -0400

    commentsToStartProtoProto

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char** av) {
+static void	init()
+{
+	//	memset 0
+	//	check config file if errors
+	return ;
+}
 
+void	parseConfigFile()
+{
+	//	call init
+	//	get info from config file
+	//	define outputs before launch
+	return ;
+}
+
+void	setUpServer(Config config)
+{
+	//	sockets
+	//	ports
+	//	Methods
+	//	adrrinfo
+	//	...
+	return ;
+}
+
+void	eventLoop()
+{
+	//	execution loop
+	// 		Wait connection (socket)
+	// 		Accepter la connexion (si nouveau client)
+	// 		Lire la requête HTTP
+	// 		Parser la requête
+	//		...
+	//		... ?
+	// 		Construire la réponse HTTP
+	// 		Envoyer la réponse
+	// 		Fermer (ou garder ouverte) la connexion
+	return ;
+}
+
+void	free()
+{
+	//	free all
+	return ;
+}
+
+int main(int ac, char** av)
+{
+	class	config = parseConfigFile();
+
+	setUpServer(config);	
+	eventLoop(config);
+	free();
 	return 0;
 }
