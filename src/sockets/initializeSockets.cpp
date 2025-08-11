f70bd078e326084318db0ed359d81f3b4cb897c5
moop250 <hugo.libine@proton.me>
2025-08-11 18:08:15 +0200

    Laid out the pseudocode for the initialization of sockets

---
+++ src/sockets/initializeSockets.cpp
@@ -0,0 +1,17 @@
+#include "../../headers/StdLibs.hpp"
+
+int initializeSockets(Config Config) {
+
+	// Sanitize the config and return a list of every unique host and port combination
+
+	for (size_t i = list.count; i >= 0; --i) {
+		// Create a socket for the current unique host and port combinatuion
+
+		// Bind the combo to a socket
+		// Start listenting to the socket
+
+		// save file descriptor to a class or something to be returned
+	}
+
+	// return status code
+};
