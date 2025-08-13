3935fc9679e5380e6b9da718ce872883acbd8ada
moop250 <hugo.libine@proton.me>
2025-08-13 14:18:41 +0200

    laid out the skeleton for sanitizeConfig Will finish once merged with the finished config branch

--- src/sockets/serverInitialization.cpp
+++ src/sockets/serverInitialization.cpp
@@ -3,11 +3,26 @@
 #include "../../headers/SocketClass.hpp"
 #include <cstddef>
 #include <exception>
+#include <string>
 #include <vector>
 
+static bool compareConfigs(std::string currentPair, std::vector<std::string> allPairs) {
+	for (int i = allPairs.size(); i > 0; --i) {
+		if (allPairs.at(i) == currentPair)
+			return false;
+	}
+	return true;
+}
+
 static std::vector<std::string> sanitizeConfig(Config serverConfig) {
-
-
+	std::vector<std::string> out;
+	for (int i = serverConfig.getServerCount(); i > 0; --i) {
+		Serverdata serv = Config.getServer(i);
+		std::string tmp;
+		tmp.append(serv.host + "|" + serv.port);
+		if (compareConfigs(tmp, out))
+			out.push_back(tmp);
+	}
 };
 
 std::vector<ServerSocket *> initalizeServer(Config serverConfig) {
