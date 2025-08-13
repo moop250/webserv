936261b34262d7412dc6a40ff24c2e699d8fd8f5
TheOrangeCake <vietfox94@gmail.com>
2025-08-13 18:23:00 +0200

    save progress, parse method, parse url no query

--- src/request/parse_request.cpp
+++ src/request/parse_request.cpp
@@ -6,26 +6,85 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 23:05:10 by hoannguy          #+#    #+#             */
-/*   Updated: 2025/08/12 23:43:11 by hoannguy         ###   ########.fr       */
+/*   Updated: 2025/08/13 18:17:42 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 #include "request_handler.hpp"
 
-int parse_request_line(Request request, std::string buffer) {
-	if (buffer.empty())
+
+int parse_method(Request& request, std::string& buffer) {
+	std::string::size_type	position;
+
+	position = buffer.find(" ");
+	if (position == std::string::npos)
 		return 400;
+	try {
+		std::string method = buffer.substr(0, position);
+		if (method == "GET" || method == "POST" || method == "DELETE") {
+			request.setMethod(method);
+			buffer.erase(0, position + 1);
+			return 0;
+		} else if (method == "HEAD" || method == "PUT"
+					|| method == "CONNECT" || method == "OPTIONS"
+					|| method == "TRACE" || method == "PATCH")
+			return 501;
+		return 400;
+	} catch (std::out_of_range& e) {
+		return 400;
+	}
 	return 0;
 }
 
-int parse_request(std::string buffer) {
+// std::string extract_URL(std::string& url, std::string::size_type position) {
+// 	std::string query;
+
+// 	try {
+// 		query = url.substr()
+// 	}
+// }
+
+int parse_URL(Request& request, std::string& buffer) {
+	std::string::size_type	position1;
+	std::string::size_type	position2;
+
+	position1 = buffer.find(" ");
+	if (position1 == std::string::npos)
+		return 400;
+	try {
+		std::string url = buffer.substr(0, position1);
+		if (url[0] == '/' || !url.compare(0, 7, "http://") || !url.compare(0, 8, "https://")) {
+			// position2 = url.find("?");
+			// if (position2 != std::string::npos) {
+			// 	std::string query = extract_URL(url, position2);
+			// 	if (query == "") {
+			// 		return 400;
+			// 	}
+			// 	request.setQuery(query);
+			// 	url.erase(position2, position1);
+			// }
+			request.setURL(url);
+			buffer.erase(0, position1 + 1);
+			return 0;
+		}
+		return 400;
+	} catch (std::out_of_range& e) {
+		return 400;
+	}
+}
+
+int parse_request(std::string& buffer) {
 	Request	request;
 	int		code = 0;
 	
-	code = parse_request_line(request, buffer);
+	if (buffer.empty())
+		return response(400);
+	code = parse_method(request, buffer);
 	if (code != 0)
-		return (-1); // construct error response here
-	
+		return response(code);
+	code = parse_URL(request, buffer);
+	if (code != 0)
+		return response(code);
 	return 0;
 }
--- src/request/tdd.cpp
+++ src/request/tdd.cpp
@@ -6,7 +6,7 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 17:59:13 by hoannguy          #+#    #+#             */
-/*   Updated: 2025/08/12 23:41:21 by hoannguy         ###   ########.fr       */
+/*   Updated: 2025/08/13 18:22:36 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "Request.hpp"
 #include "request_handler.hpp"
 
-TEST_CASE( "Request class default constructor" ) {
+TEST_CASE("Request class default constructor") {
 	Request requestDefault;
 
     REQUIRE(requestDefault.getMethod() == "");
@@ -31,17 +31,101 @@ TEST_CASE( "Request class default constructor" ) {
 	REQUIRE(requestDefault.getContentLength() == 0);
 }
 
-TEST_CASE( "Parse Request method", "[Error]") {
+TEST_CASE("Parse Request", "[Error]") {
 	Request request;
 	SECTION( "Empty request", "[General Error]") {
 		std::string buffer = "";
-		int code = parse_request_line(request, buffer);
+		int code = parse_request(buffer);
 		REQUIRE(code == 400);
 	}
-	SECTION( "No method", "[Method Error]") {
+	SECTION("Bad format", "General Error") {
+		std::string buffer = "GET/test/demo_form.phpHTTP/1.1";
+		int code = parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "");
+		REQUIRE(code == 400);
+	}
+	SECTION("No method", "[Method Error]") {
 		std::string buffer = " /test/demo_form.php HTTP/1.1";
-		int code = parse_request_line(request, buffer);
+		int code = parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "");
+		REQUIRE(code == 400);
+	}
+	SECTION("Not supported method", "[Method Error]") {
+		std::string buffer = "TRACE /test/demo_form.php HTTP/1.1";
+		int code = parse_method(request, buffer);
 		REQUIRE(request.getMethod() == "");
 		REQUIRE(code == 501);
+	}
+	SECTION("Not recognised method", "[Method Error]") {
+		std::string buffer = "FJAKSDHGF /test/demo_form.php HTTP/1.1";
+		int code = parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "");
+		REQUIRE(code == 400);
+	}
+	SECTION("No URL", "[URL Error]") {
+		std::string buffer = "GET HTTP/1.1";
+		parse_method(request, buffer);
+		int code = parse_URL(request, buffer);
+		REQUIRE(request.getURL() == "");
+		REQUIRE(code == 400);
+	}
+	SECTION("Bad URL format", "[URL Error]") {
+		std::string buffer = "GET www.google.com HTTP/1.1";
+		parse_method(request, buffer);
+		int code = parse_URL(request, buffer);
+		REQUIRE(request.getURL() == "");
+		REQUIRE(code == 400);
+	}
+}
+
+TEST_CASE("Parse request line", "[Success]") {
+	Request request;
+	SECTION("GET method", "[Success]") {
+		std::string buffer = "GET /test/demo_form.php HTTP/1.1";
+		int code = parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "GET");
+		REQUIRE(buffer == "/test/demo_form.php HTTP/1.1");
+		REQUIRE(code == 0);
+	}
+	SECTION("POST method", "[Success]") {
+		std::string buffer = "POST /test/demo_form.php HTTP/1.1";
+		int code = parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "POST");
+		REQUIRE(buffer == "/test/demo_form.php HTTP/1.1");
+		REQUIRE(code == 0);
+	}
+	SECTION("DELETE method", "[Success]") {
+		std::string buffer = "DELETE /test/demo_form.php HTTP/1.1";
+		int code = parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "DELETE");
+		REQUIRE(buffer == "/test/demo_form.php HTTP/1.1");
+		REQUIRE(code == 0);
+	}
+	SECTION("URL absolute path http no query", "[Success]") {
+		std::string buffer = "GET http://www.google.com/test HTTP/1.1";
+		parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "GET");
+		int code = parse_URL(request, buffer);
+		REQUIRE(request.getURL() == "http://www.google.com/test");
+		REQUIRE(buffer == "HTTP/1.1");
+		REQUIRE(code == 0);
+	}
+	SECTION("URL absolute path https no query", "[Success]") {
+		std::string buffer = "DELETE https://www.google.com/test HTTP/1.1";
+		parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "DELETE");
+		int code = parse_URL(request, buffer);
+		REQUIRE(request.getURL() == "https://www.google.com/test");
+		REQUIRE(buffer == "HTTP/1.1");
+		REQUIRE(code == 0);
+	}
+	SECTION("Whole line no query", "[Success]") {
+		std::string buffer = "GET /test/demo_form.php HTTP/1.1";
+		int code = parse_method(request, buffer);
+		REQUIRE(request.getMethod() == "GET");
+		// REQUIRE(buffer ==  "");
+		// REQUIRE(request.getURL() == "/test/demo_form.php");
+		// REQUIRE(request.getHttpVersion() == "HTTP/1.1");
+		REQUIRE(code == 0);
 	}
 }
