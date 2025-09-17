3a85aeb0fafc4114f9b8e9a359b4cbfce4217d86
DyoDlm <baccus.delamancha@gmail.com>
2025-09-17 10:55:45 +0200

    ok

--- curl-testing/compare-with-nginx.sh
+++ curl-testing/compare-with-nginx.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
 echo bravo, tu veux tester si le code est bon !
+
+#curl --trace-ascii debugdump.txt http://polyception.ch
+
+#host
+#curl --resolve polyception.ch:80:127.0.0.1 http://polyception.ch
+
+#port number
+#curl http://polyception.ch:1234 # idk how it works
+
+# GET & POST
+#curl https://curl.se
+# with headers
+#curl https://curl.se --head
+
+# it sends first a head then a get
+#curl -I http://example.com --next http://example.com
+
+# it sends a POST then a GET
+#curl -d score=10 http://example.com/post.cgi --next http://example.com/results.html
+
+# it's prototype as : 
+# <form method="GET" action="junk.cgi">
+#  <input type=text name="birthyear">
+#  <input type=submit name=press value="OK">
+#</form>
+#
+#and
+#<form method="POST" action="junk.cgi">
+#  <input type=text name="birthyear">
+#  <input type=submit name=press value=" OK ">
+#</form>
+
+
--- curl-testing/nginx/convert.cpp
+++ curl-testing/nginx/convert.cpp
@@ -14,21 +14,21 @@ void	replace(std::string &s, std::string to_find, std::string to_replace)
 	std::cout << "New line\n";
 }
 
-void	convert(std::ifstream &file, bool to_webserv)
+std::string	convert(std::ifstream &file, bool to_webserv)
 {
 	const int			nbTokens = 12;
 	std::string			line;
 	std::stringstream	to_write;
 	std::string	myTokens[] = {
-		"<listen>", "<host>", "<server_name>", "<html_index>",
+		"<listen>", "<host>", "<server_name>",
 		"<root>", "<client_max_body_size>", "<autoindex>",
 		"<error_page>", "<upload_storage>", "<CGI>", "<allow>",
-		"<return>"
+		"<return>", "<html_index>"
 	};
 	std::string	nginxTokens[] = {
-		"listen", "host", "server_name", "index", "root",
-		"client_max_body_size", "autoindex", "error_page",
-		"upload", "cgi", "allow", "return"
+		"listen ", "host ", "server_name ", "root ",
+		"client_max_body_size ", "autoindex ", "error_page ",
+		"upload ", "cgi ", "allow ", "return ", "index "
 	};
 
 	while (std::getline(file, line))
@@ -50,19 +50,21 @@ void	convert(std::ifstream &file, bool to_webserv)
 		to_write << '\n';
 	}
 
-	std::ofstream	newFile("converted.config");
-	newFile << to_write.str().c_str();
-	return ;
+	return to_write.str();
 }
 
 int	main(int ac, char **av)
 {
+	std::string	name = av[1];
+	std::string	webserv_convert = av[2];
+
 	std::cout << "Converting file..." << std::endl;
 
 	if (ac != 3)
 	{
 		std::cout << "Bad arguments...\n"
 			<< "try : ./convert [filename.config] [0 | 1]" << std::endl;
+		std::cout << "0 to nginx, 1 to webserv\n";
 		return (1);
 	}
 
@@ -73,7 +75,25 @@ int	main(int ac, char **av)
 		std::cout << "File does not exist\n";
 		return (1);
 	}
-	convert(file, atoi(av[2]));
+	if (name.find(".config") == std::string::npos)
+	{
+		std::cout << "File given doesn't respect '.config' format\n";
+		return (1);
+	}
+	std::cout << "Converting file ";
+	name.erase(name.find(".config"), 7);
 	
+	if (webserv_convert == "0") {
+		std::cout << "to nginx format...\n"; name.append("_nginx.config");
+	}
+	else if (webserv_convert == "1") {
+		std::cout << "to webserv format...\n"; name.append("_webserv.config");
+	}
+	else
+		std::cout << "Where is the option?\n";
+
+	std::ofstream	newFile(name.c_str());
+
+	newFile << convert(file, atoi(av[2]));
 	return (0);
 }
