dcf845384f1b1e1e4c295c946668b1a1c874f460
moop250 <hugo.libine@proton.me>
2025-10-17 18:25:01 +0200

    read

--- webserv/src/sockets/genFDHandling.cpp
+++ webserv/src/sockets/genFDHandling.cpp
@@ -2,8 +2,24 @@
 #include "../../headers/Sockets.hpp"
 #include "../../headers/Connection.hpp"
 #include "../../headers/request_handler.hpp"
+#include <string>
 
-int handleFDIn(t_fdInfo *fdInfo, int fd) {
+int handleFDIn(t_fdInfo *fdInfo, int fd, std::string *output) {
+	char	buffer[4096];
+	ssize_t	n = 0;
+		
+	n = read(fd, buffer, sizeof(buffer));
+	if (n > 0) {
+		output->append(buffer, n);
+		return 1;
+	}
+	if (n == 0) {
+		return 0;
+	}
+	if (n < 0) {
+		close(fd);
+		return -1;
+	}
 
 	return 0;
 }
