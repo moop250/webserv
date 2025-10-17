4c9bc07ea4272dfb0b1c2de1b59dd726957a10a1
moop250 <hugo.libine@proton.me>
2025-10-17 15:42:49 +0200

    starting a block

---
+++ webserv/src/sockets/genFDHandling.cpp
@@ -0,0 +1,14 @@
+#include "../../headers/SocketClass.hpp"
+#include "../../headers/Sockets.hpp"
+#include "../../headers/Connection.hpp"
+#include "../../headers/request_handler.hpp"
+
+int handleFDIn(t_fdInfo *fdInfo, int fd) {
+
+	return 0;
+}
+
+int handleFDOut(t_fdInfo *fdInfo, int fd) {
+
+	return 0;
+}
