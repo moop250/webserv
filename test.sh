bf1c2e663b5b524d7202de67af1c74a0a60146b8
dyodlm <baccus.delamancha@gmail.com>
2025-10-21 12:31:34 +0200

    Test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 display() {	# takes title for arg
 	echo ////////////////////////////////
 	echo 
@@ -178,18 +177,3 @@ echo
 if [ -f files ] ; then
 	rm files 
 fi
-=======
-
-./tests/stress1.sh &
-pid1=$!
-./tests/stress2.sh &
-pid2=$!
-
-
-sleep 5 
-
-kill $pid1
-kill $pid2
-
-#curl 127.0.0.1:8001/html/accueil.html
->>>>>>> main
